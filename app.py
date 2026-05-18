c6fada6fb0542d5e6c09b0de5efbf305cb2f461e
rbx86 <210249546+rbx86@users.noreply.github.com>
2026-05-18 16:47:41 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect
+import random
+import string
+
+app = Flask(__name__)
+
+url_map = {}
+
+def generate_code():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    short_url = None
+
+    if request.method == 'POST':
+
+        long_url = request.form['url']
+
+        code = generate_code()
+
+        url_map[code] = long_url
+
+        short_url = request.host_url + code
+
+    return render_template('index.html', short_url=short_url)
+
+@app.route('/<code>')
+def redirect_url(code):
+
+    if code in url_map:
+        return redirect(url_map[code])
+
+    return "URL not found"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
